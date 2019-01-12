af3c74bc6082b5cdd4bfa27555d557a0811e9916
amyngb <amyngbedinghaus@gmail.com>
2019-01-12 11:11:31 -0500

    Allow a user to post an article and display, using session data

--- NewsfeedRepo/NewsfeedRepo/Controllers/HomeController.cs
+++ NewsfeedRepo/NewsfeedRepo/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using NewsfeedRepo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,18 +15,40 @@ namespace NewsfeedRepo.Controllers
 			return View();
 		}
 
-		public ActionResult About()
+		[HttpGet]
+		public ActionResult CreateArticle()
 		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
+			var article = new Article();
+			return View(article);
 		}
 
-		public ActionResult Contact()
+		[HttpPost]
+		public ActionResult CreateArticle(Article article)
 		{
-			ViewBag.Message = "Your contact page.";
+			var articleToAdd = new Article();
+			articleToAdd.Author	= User.Identity.Name;
+			articleToAdd.DatePosted = DateTime.Now;
+			articleToAdd.Body = article.Body;
+			articleToAdd.Title = article.Title;
+			
+			AddArticle(articleToAdd);
+			return Redirect("/");
+		}
 
-			return View();
+		private void AddArticle(Article article)
+		{
+			List<Article> articleList;
+			if ((List<Article>)Session["articles"] == null)
+			{
+				articleList = new List<Article>();
+			}
+			else
+			{
+				articleList = (List<Article>)Session["articles"];
+			}
+			
+			articleList.Add(article);
+			Session["articles"] = articleList;
 		}
 	}
 }
---
+++ NewsfeedRepo/NewsfeedRepo/Models/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewsfeedRepo.Models
+{
+	public class Article
+	{
+		[Required]
+		public string Title { get; set; }
+		[Required]
+		public string Author { get; set; }
+		public string Body { get; set; }
+		public DateTime DatePosted { get; set; }
+		public DateTime DateRevised { get; set; }
+	}
+}
